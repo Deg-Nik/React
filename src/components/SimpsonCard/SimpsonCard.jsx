da8fc974c620e2b1eba3e04275297a9e6982b5da
Nikolai <degnikolaj@gmail.com>
2025-11-14 07:54:13 +0100

    new

--- src/components/SimpsonCard/SimpsonCard.jsx
+++ src/components/SimpsonCard/SimpsonCard.jsx
@@ -1,77 +1,57 @@
 import "./styles.css";
 
-function SimpsonCard() {
-  const person = {
-    firstName: "Gomer",
-    lastName: "Simpson",
-    age: 39,
-    city: "Springfield",
-    street: "Evergreen Alley 742",
-    profession: "Inspector at a nuclear power plant",
-    hobby: "Meal, Beer and Television set",
-    isMarried: true,
-    wife: "Marge",
-    children: "Bard, Lisa and Maggie",
-    avatar:
-      "https://cs9.pikabu.ru/post_img/2018/12/03/9/1543850691181871789.jpg",
-  };
+// props это возможность передавать данные из родителя к дочерним - он является обьектом
+// function SimpsonCard(props) {
+function SimpsonCard({ firstName, lastName, age, profession, hobby, avatar }) {
+  // console.log(props);
 
-  const getMarriedStatus = (isMarried) => {
-    if (isMarried) {
-      return "yes";
-    } else {
-      return "no";
-    }
-  };
+  // const person = {
+  //   firstName: "Gomer",
+  //   lastName: "Simpson",
+  //   age: 39,
+  //   city: "Springfield",
+  //   street: "Evergreen Alley 742",
+  //   profession: "Inspector at a nuclear power plant",
+  //   hobby: "Meal, Beer and Television set",
+  //   isMarried: true,
+  //   wife: "Marge",
+  //   children: "Bard, Lisa and Maggie",
+  //   avatar:
+  //     "https://cs9.pikabu.ru/post_img/2018/12/03/9/1543850691181871789.jpg",
+  // };
+
+  // const{firstName, lastName, age, profession, hobby, avatar} = props;
+
+  // const getMarriedStatus = (isMarried) => {
+  //   if (isMarried) {
+  //     return "yes";
+  //   } else {
+  //     return "no";
+  //   }
+  // };
 
   return (
-    <div className="card_component">
     <div className="person_card">
       <p className="card_title">Person</p>
-      <img className="avatar" src= {person.avatar} alt="Person Avatar"/>
+      <img className="avatar" src={avatar} alt="Person Avatar" />
       <p className="person_info">
         <span className="label">First Name: </span>
-        {person.firstName}
+        {firstName}
       </p>
       <p className="person_info">
         <span className="label">Last Name: </span>
-        {person.lastName}
-      </p>
-      <p className="person_info">
-        <span className="label">Age: </span> 
-        {person.age}
-      </p>
-      <p className="person_info">
-        <span className="label">City: </span> 
-        {person.city}
-      </p>
-      <p className="person_info">
-        <span className="label">Street: </span> 
-        {person.street}
+        {lastName}
       </p>
       <p className="person_info">
         <span className="label">Profession: </span>
-        {person.profession}
+        {profession}
       </p>
       <p className="person_info">
         <span className="label">Hobby: </span>
-        {person.hobby}
-      </p>
-      <p className="person_info">
-        <span className="label">Is Married: </span> 
-        {getMarriedStatus (person.isMarried)}
-      </p>
-      <p className="person_info">
-        <span className="label">Wife: </span> 
-        {person.wife}
-      </p>
-      <p className="person_info">
-        <span className="label">Children: </span> 
-        {person.children}
+        {hobby}
       </p>
     </div>
-  </div>
-  )
-};
+  );
+}
 
 export default SimpsonCard;
--- src/lessons/lesson_03/Lesson_03.jsx
+++ src/lessons/lesson_03/Lesson_03.jsx
@@ -1,12 +1,67 @@
 import "./styles.css";
 import SimpsonCard from "../../components/SimpsonCard/SimpsonCard";
+import Button from "../../components/Button/Button";
 
-function Lesson_03 () {
+function Lesson_03() {
+  const homerSimpson = {
+    firstName: "Homer",
+    lastName: "Simpson",
+    profession: "Nuclear Security Specialist",
+    hobby: "Bear, TV, FastFood",
+    avatar:
+      "https://upload.wikimedia.org/wikipedia/en/0/02/Homer_Simpson_2006.png",
+  };
+  const margeSimpson = {
+    firstName: "Marge",
+    lastName: "Simpson",
+    profession: "No Job",
+    hobby: "coocking",
+    avatar: "https://upload.wikimedia.org/wikipedia/en/0/0b/Marge_Simpson.png",
+  };
+  const bartSimpson = {
+    firstName: "Bart",
+    lastName: "Simpson",
+    profession: "No job",
+    hobby: "Skate",
+    avatar:
+      "https://upload.wikimedia.org/wikipedia/en/a/aa/Bart_Simpson_200px.png",
+  };
   return (
     <div className="lesson_03">
-      <SimpsonCard/>
-      <SimpsonCard/>
-      <SimpsonCard/>
+      <div className="cards_container">
+        <SimpsonCard
+          firstName={homerSimpson.firstName}
+          lastName={homerSimpson.lastName}
+          profession={homerSimpson.profession}
+          hobby={homerSimpson.hobby}
+          avatar={homerSimpson.avatar}
+        />
+        <SimpsonCard
+          firstName={margeSimpson.firstName}
+          lastName={margeSimpson.lastName}
+          profession={margeSimpson.profession}
+          hobby={margeSimpson.hobby}
+          avatar={margeSimpson.avatar}
+        />
+        <SimpsonCard
+          firstName={bartSimpson.firstName}
+          lastName={bartSimpson.lastName}
+          profession={bartSimpson.profession}
+          hobby={bartSimpson.hobby}
+          avatar={bartSimpson.avatar}
+        />
+      </div>
+      <div className="buttons_container">
+        <div className="button_control">
+          <Button name="Send Request" />
+        </div>
+        <div className="button_control">
+          <Button name="Get User Data" />
+        </div>
+        <div className="button_control">
+          <Button />
+        </div>
+      </div>
     </div>
   );
 }
